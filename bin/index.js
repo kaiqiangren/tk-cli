115e498b55beab05eac83722fdd985552961971b
renkaiqiang <1006042475@qq.com>
2020-05-27 15:36:07 +0800

    feature:add document

--- bin/index.js
+++ bin/index.js
@@ -9,13 +9,22 @@ const download = require('download-git-repo');
 // 定义版本
 program.version(require('../package').version);
 
+// description主要用于 tk --help中的说明
 program
     .command('init')
-    .description('run init commands')
+    .description('初始化项目的启动命令')
     .action(async () => {
       const spinner = ora("Downloading...");
       const answer = await inquirer.prompt(question)
       const { projectName, templateType } = answer;
+      // const nextAnswer = await inquirer.prompt([
+      //   {
+      //     type:'input',
+      //     name:'nextQuestion',
+      //     message:'下一个问题:',
+      //     default: 'next question!'
+      //   },
+      // ])
       spinner.start();
       download(
           'github:kaiqiangren/vue-next-ts-preview',
